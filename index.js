7691515db94614d6849d724bbf89168045226de8
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-08-08 19:15:36 -0400

    added const questions for readme generator

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const inqurier = require('inquirer');
 const fs = require('fs'); //file system
 const generateMarkdown = require('./generateMarkdown')
-console.log("Welcome to my README Generator!")
+console.log("Hello, welcome to my README Generator!")
 console.log("Answer the following questions to generate a high quality README for your project.")
 
 // TODO: Create an array of questions for user input
 const questions = [
+    
+    //Title
     {
     type: 'input',
     name: 'title',
@@ -20,6 +22,150 @@ const questions = [
         }
     }
     },
+
+    //Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Add a description for your project.',
+        validate: your_description => {
+            if (your_description) {
+                return true;
+            } else {
+                console.log('Enter a description to continue!');
+                return false;
+            }
+        }
+    },
+
+    
+    //Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+        validate: your_installation => {
+            if (your_installation) {
+                return true;
+            } else {
+                console.log('Enter the steps of installation to continue.')
+                return false;
+            }
+        }
+
+    },
+
+
+    //Usage Info
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+        validate: your_usage => {
+            if (your_usage) {
+                return true;
+            } else {
+                console.log('Enter the information on how to use this project.')
+                return false;
+            }
+        }
+
+    },
+
+
+        //License
+
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Choose a license that will best suit your project.',
+            choices: ['MPL 2.0', 'GNU', 'Apache', 'MIT', 'None of the above'],
+            validate: your_licence => {
+                if (your_licence) {
+                    return true;
+                } else {
+                    console.log('Choose a license.')
+                    return false;
+                }
+            }
+    
+        },
+
+
+
+        //Contribution
+
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How can users contribute to this project?',
+            validate: your_contribution => {
+                if (your_contribution) {
+                    return true;
+                } else {
+                    console.log('Enter the information on how users can contribute to this project.')
+                    return false;
+                }
+            }
+    
+        },
+
+
+
+        //Test
+
+        {
+            type: 'input',
+            name: 'test',
+            message: 'How does the user test this project?',
+            validate: your_test => {
+                if (your_test) {
+                    return true;
+                } else {
+                    console.log('Explain how the user tests this project.')
+                    return false;
+                }
+            }
+    
+        },
+
+
+        //GitHub Username
+
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github username?',
+            validate: github_input => {
+                if (github_input) {
+                    return true;
+                } else {
+                    console.log('It is required you enter your Github username. Try again.')
+                    return false;
+                }
+            }
+    
+        },     
+        
+
+        //Email
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your email so users may be able to ask you questions.',
+            validate: email_input => {
+                if (email_input) {
+                    return true;
+                } else {
+                    console.log('Enter your email.')
+                    return false;
+                }
+            }
+    
+        },   
+
 ];
 
 // TODO: Create a function to write README file
