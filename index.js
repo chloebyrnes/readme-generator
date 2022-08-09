f19e7d48907832286d5b41aaaa005340cb372066
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-08-08 20:08:34 -0400

    index.js done for now, need to complete generateMarkdown.js

--- index.js
+++ index.js
@@ -169,10 +169,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("All done! You can now preview your README file!");
+        });
+    };
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then(function(userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
